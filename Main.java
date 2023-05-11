8380cd3d73d7b2de6e5a2b41e564ab2270c1cfbb
Yatzari <2130039@upv.edu.mx>
2023-05-11 12:07:25 -0600

    Funciona

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ class Main {
 
     public static void main(String[] args) {
     
-    	//La clase int es un tipo de dato primitivo y Integer es un tipo de dato de clase.
-    	//Integer es un valor de tipo entero.
+    	//La clase int es un tipo de dato primitivo y Integer es un tipo de dato de clase
+    	//Integer es un valor de tipo entero
     	int a = Integer.parseInt(args[0]);
     	int b = Integer.parseInt(args[1]);
     	
-    	//Se crean objetos y el código se llama instancia de la clase
+    	//Se crean objetos y el codigo se llama instancia de la clase
     	SumaDosNumeros obj = new SumaDosNumeros();
     	
     	System.out.println(obj.suma(a, b));
